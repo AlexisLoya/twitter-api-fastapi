5e43e6c6d9dcd73482e428883f7e715311ec630b
AlexisLoya <alexloy117@gmail.com>
2022-06-02 01:28:23 -0500

    tweets almost done

--- routers/tweet.py
+++ routers/tweet.py
@@ -1,9 +1,9 @@
 """Tweet Routers"""
 # Python
 from typing import List
-
+import json
 # FastAPI
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 
 # Models
 from models import Tweet
@@ -22,8 +22,18 @@ router.tags = ["Tweets"]
     summary='Show all tweets',
 )
 def show_all_tweets():
-    pass
+    """
+    This path operation shows all tweets in the app
 
+    Parameters: 
+        -
+
+    Returns a json list with all users in the app, with the following keys: 
+        - tweet_id: UUID
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f: 
+        results = json.loads(f.read())
+        return results
 
 @router.post(
     path='/',
@@ -31,8 +41,38 @@ def show_all_tweets():
     status_code=status.HTTP_201_CREATED,
     summary='post a tweet',
 )
-def post_a_tweets():
-    pass
+def post_a_tweets(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+    This path operation post a tweet in the app
+    Parameters: 
+        - Request body parameter
+            - tweet: Tweet
+    
+    Returns a json with the basic tweet information: 
+        tweet_id: UUID  
+        content: str    
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        tweet_dict["by"]["created_at"] = str(tweet_dict["by"]["created_at"])
+        tweet_dict["by"]["updated_at"] = str(tweet_dict["by"]["updated_at"])
+        tweet_dict["by"]["gender"] = str(tweet_dict["by"]["gender"])[8::]
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet    
+
+
 
 
 
